ea61b9d207758da81c26026e457b9a63e7deb604
ms2300 <matt.sewall@gmail.com>
2019-03-18 11:39:47 -0700

    I dislike read_the_docs

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
-import guzzle_sphinx_theme
-
 
 # -- Project information -----------------------------------------------------
 
@@ -77,17 +75,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
-
-# Register the theme as an extension to generate a sitemap.xml
-extensions.append("guzzle_sphinx_theme")
-
-# Guzzle theme options (see theme.conf for more information)
-html_theme_options = {
-    # Set the name of the project to appear in the sidebar
-    "project_nav_name": "ventana",
-}
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
